4e2d0d501a40d903242464bf5dd588b709b4c90e
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2017-02-25 04:29:25 +0530

    Add couple.py

---
+++ couple.py
@@ -0,0 +1,24 @@
+'''COUPLE'''
+
+class Couple(object):
+
+    '''COUPLE CLASS'''
+
+    def __init__(self, boy, girl):
+        '''DEFAULT CONSTRUCTOR'''
+        self.boy = boy
+        self.girl = girl
+        self.happiness = 0
+        self.compatibility = 0
+        self.gift_list = []
+
+    def set_happiness(self):
+        '''CALCULATES COUPLE'S HAPPINESS'''
+        self.happiness = self.boy.happiness + self.girl.happiness
+
+    def set_compatibility(self):
+        '''CALCULATES COUPLE'S COMPATIBILITY'''
+        money_diff = (self.boy.budget - self.girl.mcost)
+        attr_diff = abs(self.boy.attr - self.girl.attr)
+        intel_diff = abs(self.boy.intel - self.girl.intel)
+        self.compatibility = money_diff + attr_diff + intel_diff
