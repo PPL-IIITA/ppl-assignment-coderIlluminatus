19070e2be561ed251a299e71dc281d0beac7ad79
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2017-02-25 04:38:30 +0530

    Commit question_1.py, test_utility.py

--- question_1.py
+++ question_1.py
@@ -1,6 +1,6 @@
 '''QUESTION 1'''
 
-from algorithms import random_generator_people
+from test_utility import random_generator_people
 from algorithms import make_couples
 
 random_generator_people()
---
+++ test_utility.py
@@ -0,0 +1,72 @@
+'''TEST UTILITY TO RANDOMLY GENERATE csv FILES'''
+
+from random import randint
+from random import choice
+import csv
+
+def create(filename, datalist):
+    '''CREATES csv ROWS FROM GIVEN DATA SET'''
+    newfile = open(filename, 'w')
+    writer = csv.writer(newfile, delimiter=',')
+
+    for i in datalist:
+        writer.writerow(i)
+
+def random_generator_people():
+    '''GENERATES RANDOM LIST OF BOYS AND GIRLS'''
+    n_boys = randint(10, 20)
+    n_girls = randint(10, n_boys-1)
+
+    boys_nature = ['Miser', 'Generous', 'Geek']
+    girls_nature = ['Choosy', 'Normal', 'Desparate']
+
+    boys_list = ([('Boy '+str(i + 1),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 choice(boys_nature))
+                  for i in range(n_boys)])
+
+    girls_list = ([('Girl '+str(i + 1),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 randint(1, 100),\
+                 choice(girls_nature))
+                   for i in range(n_girls)])
+
+    create('boys.csv', boys_list)
+    create('girls.csv', girls_list)
+
+def random_generator_gifts():
+    '''GENERATES RANDOM LIST OF BOYS AND GIRLS'''
+    n_gifts = randint(100, 200)
+
+    gift_type = ['Essential', 'Luxury', 'Utility']
+    gifts_list = []
+
+    for i in range(n_gifts):
+        current_gift_type = choice(gift_type)
+        if current_gift_type == 'Essential':
+            gifts_list[i] = ('Gift '+str(i + 1),\
+                            randint(1, 100),\
+                            randint(1, 100),\
+                            choice(gift_type))
+        elif current_gift_type == 'Luxury':
+            gifts_list[i] = ('Gift '+str(i + 1),\
+                            randint(1, 100),\
+                            randint(1, 100),\
+                            choice(gift_type),\
+                            randint(1, 5),\
+                            randint(1, 5))
+        elif current_gift_type == 'Utility':
+            gifts_list[i] = ('Gift '+str(i + 1),\
+                            randint(1, 100),\
+                            randint(1, 100),\
+                            choice(gift_type),\
+                            randint(1, 10),\
+                            randint(1, 10))
+
+    create('gifts.csv', gifts_list)
