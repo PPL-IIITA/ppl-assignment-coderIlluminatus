fd1bb4cd22da1d8257a6bca4debfd926d53dae4a
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2017-02-26 01:54:28 +0530

    Base class

---
+++ gifts/gift.py
@@ -0,0 +1,10 @@
+'''GIFT'''
+
+class Gift(object):
+
+    '''BASE CLASS'''
+
+    def __init__(self, price, value):
+        '''DEFAULT CONSTRUCTOR'''
+        self.price = price
+        self.value = value
