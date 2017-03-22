4b8e2cfe72bd1b41faa09264babdcab28340189d
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2017-03-22 08:11:09 +0530

    Add question 3 main

---
+++ question_3.py
@@ -0,0 +1,25 @@
+'''QUESTION 3'''
+
+import pickle
+
+from algorithms import make_couples
+from algorithms import give_gifts
+from algorithms import print_gifts
+from algorithms import print_happy_couples
+from algorithms import print_compatible_couples
+
+make_couples(True)
+couples_list = pickle.load(open("couple.p", "rb"))
+
+if len(couples_list) >= 1:
+    give_gifts(True, couples_list)
+
+    print_gifts(couples_list)
+
+    from random import randint
+    k = randint(1, len(couples_list))
+    print_happy_couples(couples_list, int(k))
+    print_compatible_couples(couples_list, int(k))
+
+else:
+    print("NO COUPLES FOUND.")
